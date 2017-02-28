87bf49c264dfe05011701a618b3f0ff245a23126
INF112v16-yst003 <yijun@staut.land>
2017-02-28 09:48:44 +0100

    2 funksjoner er skrevet.

---
+++ Calculator.js
@@ -0,0 +1,14 @@
+function squareNumber(i)
+{	
+    var numberAS = Number(i)*Number(i);
+
+    var S = "The result of squaring " + i + " is " + numberAS + "<br>";
+    return S;
+}
+
+function halfNUmber(i)
+{
+	var numberAfter = i/2;
+	return "Half of " + i + "is " + numberAfter;
+}
+
