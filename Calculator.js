2e0c852d4c70233eb765a21a752de476ea60a34e
INF112v16-yst003 <yijun@staut.land>
2017-02-28 13:28:45 +0100

    oppdatert func til å returnere tall istedenfor string.

--- Calculator.js
+++ Calculator.js
@@ -1,14 +1,12 @@
 function squareNumber(i)
 {	
     var numberAS = Number(i)*Number(i);
-
-    var S = "The result of squaring " + i + " is " + numberAS + "<br>";
-    return S;
+    return numberAS;
 }
 
 function halfNUmber(i)
 {
-	var numberAfter = i/2;
-	return "Half of " + i + "is " + numberAfter;
+	var numberAfter = Number(i)/2;
+	return numberAfter;
 }
 
